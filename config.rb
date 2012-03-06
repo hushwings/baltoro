b5125fd01564e791c4b7869b68990cb6f2fb5244
hushwings <prodreamer29@gmail.com>
2012-03-07 00:18:59 +0800

    initial source files

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/baltoro/"
+http_images_path = "/baltoro/images"
+http_fonts_path = "/baltoro/fonts"
+css_dir = "public/baltoro/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
---
+++ source/_posts/2012-03-06-qi-dong-blog.markdown
@@ -0,0 +1,7 @@
+---
+layout: post
+title: "启动Blog"
+date: 2012-03-06 21:38
+comments: true
+categories: 
+---
